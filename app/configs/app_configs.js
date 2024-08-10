548bacf9624d322f223c950a5bce25d26b3764ab
Declan Mould <44209534+TheGamerzs@users.noreply.github.com>
2024-08-10 15:49:58 +0100

    fix: Update CFX links in app_configs

--- app/configs/app_configs.js
+++ app/configs/app_configs.js
@@ -1,24 +1,24 @@
 const AppConfigs = {
-	server_order: [
-		"http://v1.api.tycoon.community/main",
-		"http://v1.api.tycoon.community/beta"
-	],
-	permissions: {
-		OWNER: 3,
-		MANAGER: 2,
-		MEMBER: 1,
-		GUEST: 0
-	},
-	ttpermissions: {
-		ADMIN: 3,
-		SEARCH_ALL: 2,
-		SEARCH_OTHERS: 0,
-		SEE_SELF: 0,
-		UNLINKED: -1
-	},
-	CEOID: "404650985529540618",
-	CTOID: "330000865215643658",
-	token_max_age: 1209600
+    server_order: [
+        "http://tycoon-2epova.users.cfx.re/status",
+        "http://tycoon-njyvop.users.cfx.re/status",
+    ],
+    permissions: {
+        OWNER: 3,
+        MANAGER: 2,
+        MEMBER: 1,
+        GUEST: 0,
+    },
+    ttpermissions: {
+        ADMIN: 3,
+        SEARCH_ALL: 2,
+        SEARCH_OTHERS: 0,
+        SEE_SELF: 0,
+        UNLINKED: -1,
+    },
+    CEOID: "404650985529540618",
+    CTOID: "330000865215643658",
+    token_max_age: 1209600,
 };
 
 // CFX Links
